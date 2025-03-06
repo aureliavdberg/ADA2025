2a5e0ee7511aafff9119ff1d14b84015cee18f98
Aurelia van den Berg <82882706+aureliavdberg@users.noreply.github.com>
2025-03-06 12:27:47 +0100

    lab 4 live

--- lab4/deliveryservicewithopenapi/app.py
+++ lab4/deliveryservicewithopenapi/app.py
@@ -11,8 +11,8 @@ from db import Base, engine
 logging.basicConfig(level=logging.INFO)
 app = connexion.App(__name__, specification_dir="openapi/")
 Base.metadata.create_all(engine)
-app.add_api('delivery-service-api.yaml',
-            arguments={'title': 'Deliver Service API'})
+app.add_api('delivery-service-api.yaml', # Name of the open api
+            arguments={'title': 'Deliver Service API'}) # Replace the title from yaml file
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0')
